3274d91790b284f94843c46253089b7400148bf6
MariaBruning15 <mariaeduardafbruning@gmail.com>
2025-05-10 14:31:21 -0300

    exercicio1 bubble sort

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,5 +1,33 @@
 public class Exercicio1{
     public static void main(String[] args) {
-    
+        int[] vetor = {3, 4, 9, 2, 5, 8, 2, 1, };
+        
+    }
+    public void bubbleSort(int[] vetor){
+        ImprimirVetor impressão = new ImprimirVetor();
+        		int tamanho = vetor.length;
+		int cont = 0;
+		boolean trocou = false;
+		for (int x = 0; x<tamanho - 1; x++) {
+			
+			for (int y = 0; y<tamanho - 1; y++) {
+				cont ++;
+				if(vetor[y] > vetor[y+1]) {
+					
+					int aux = vetor[y];
+					vetor[y] = vetor[y+1];
+					vetor[y+1] = aux;
+					trocou = true;
+					
+				}
+				
+			}
+			
+			if (!trocou)
+				break;
+		}
+        impressão.imprimir(vetor);
+		
+    }
 }
-}
+
---
+++ ImprimirVetor.java
@@ -0,0 +1,9 @@
+public class ImprimirVetor {
+    public static void imprimir(int vetor[]) {
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		System.out.println("");
+	}
+
+}
